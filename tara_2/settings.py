8e13a7d110670063d247b4355498e03d15b77ce5
albertthomas2205 <122583969+albertthomas2205@users.noreply.github.com>
2026-03-18 12:22:26 +0530

    dev

--- tara_2/settings.py
+++ tara_2/settings.py
@@ -118,24 +118,24 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'thara_gen_one',
-        'USER': 'thara_user',
-        'PASSWORD': 'thara_one',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'thara_gen_one',
+#         'USER': 'thara_user',
+#         'PASSWORD': 'thara_one',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
